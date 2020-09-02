8ab9eab9136c3a9e2d4258e3f5e1a2079a47e8da
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2020-09-02 11:32:57 +0800

    Updates

---
+++ precache-manifest.7db98de55daa20a57d7a8fdf42463f64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed0bb7b7c0cb50930fb7357a9510a23",
+    "url": "/vcas-react-app-api/index.html"
+  },
+  {
+    "revision": "c4b202e9ac386f63ec15",
+    "url": "/vcas-react-app-api/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "0b0950570c005dcde183",
+    "url": "/vcas-react-app-api/static/css/main.4bdc3e1c.chunk.css"
+  },
+  {
+    "revision": "c4b202e9ac386f63ec15",
+    "url": "/vcas-react-app-api/static/js/2.14ab5fea.chunk.js"
+  },
+  {
+    "revision": "3ece35ffdaee3775dcd439b7a638857b",
+    "url": "/vcas-react-app-api/static/js/2.14ab5fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0950570c005dcde183",
+    "url": "/vcas-react-app-api/static/js/main.ec77db5a.chunk.js"
+  },
+  {
+    "revision": "96c5f3ec10f93ca1c458",
+    "url": "/vcas-react-app-api/static/js/runtime-main.d26a6032.js"
+  },
+  {
+    "revision": "ecf136b4e54af7d356eab72795a749b2",
+    "url": "/vcas-react-app-api/static/media/vessel_marker.ecf136b4.png"
+  },
+  {
+    "revision": "e83d5b18b03ea8a22d3a14aa7c7cb29f",
+    "url": "/vcas-react-app-api/static/media/warning_red.e83d5b18.png"
+  }
+]);
